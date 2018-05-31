959f53c1430b18f36263d179303d8e6c11c00c05
simalexan <alexander.simovic@gmail.com>
2018-06-01 00:04:54 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+
+## Api Gateway -> Lambda (Update) -> DynamoDB
+
+#### Description
+This is a Lambda function that updates an item in the DynamoDB database, based on the API Gateway request data. Supports CORS. Written in Node.js. It's a Nuts & Bolts application component for AWS Serverless Application Repository.
---
+++ index.js
@@ -0,0 +1,51 @@
+const AWS = require('aws-sdk');
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const processResponse = require('./process-response');
+const TABLE_NAME = process.env.TABLE_NAME;
+const IS_CORS = process.env.IS_CORS;
+const PRIMARY_KEY = process.env.PRIMARY_KEY;
+
+exports.handler = (event) => {
+    if (event.httpMethod === 'OPTIONS') {
+		return Promise.resolve(processResponse(IS_CORS));
+	}
+    if (!event.body) {
+        return Promise.resolve(processResponse(IS_CORS, 'invalid', 400));
+    }
+    const editedItemId = event.pathParameters.id;
+    if (!editedItemId) {
+        return Promise.resolve(processResponse(IS_CORS, 'invalid', 400));
+    }
+
+    const editedItem = JSON.parse(event.body);
+    const editedItemProperties = Object.keys(editedItem);
+    if (!editedItem || editedItemProperties.length < 1) {
+        return Promise.resolve(processResponse(IS_CORS, 'no args provided', 400));
+    }
+
+    const key = {};
+    key[PRIMARY_KEY] = editedItemId;
+
+    const firstProperty = editedItemProperties.splice(0,1);
+    let params = {
+        TableName: TABLE_NAME,
+        Key: key,
+        UpdateExpression: `set ${firstProperty} = :${firstProperty}`,
+        ExpressionAttributeValues: {},
+        ReturnValues: 'UPDATED_NEW'
+    }
+    params.ExpressionAttributeValues[`:${firstProperty}`] = editedItem[firstProperty];
+
+    editedItemProperties.forEach(property => {
+        params.UpdateExpression += `, ${property} = :${property}`;
+        params.ExpressionAttributeValues[`:${property}`] = editedItem[property];
+    });
+
+    return dynamoDb.update(params)
+    .promise()
+    .then(() => (processResponse(IS_CORS)))
+    .catch(err => {
+        console.log(err);
+        return processResponse(IS_CORS, 'dynamo-error', 500);
+    });
+};
