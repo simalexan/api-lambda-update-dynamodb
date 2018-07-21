636d179d379358b9d8a4ded6be629f0f80b13d31
Aleksandar Simovic <alexander.simovic@gmail.com>
2018-07-21 22:58:56 +0200

    process response

--- process-response.js
+++ process-response.js
@@ -2,7 +2,7 @@ module.exports = (isCors, body, requestedCode) => {
     const code = requestedCode || (body ? 200 : 204);
     const headers = isCors ? {
         'Access-Control-Allow-Headers': 'Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token',
-        'Access-Control-Allow-Methods': 'OPTIONS,POST',
+        'Access-Control-Allow-Methods': 'OPTIONS,POST,PUT',
         'Access-Control-Allow-Origin': process.env.CORS_ORIGIN,
         'Access-Control-Max-Age': '86400',
         'Content-Type': 'application/json'
